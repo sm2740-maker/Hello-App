7bf502f140ed6b22f2c6e14e3675e8967dd0a645
Shayon Mazumder <sm2740@srmist.edu.in>
2026-03-18 08:09:33 +0530

    UC3: Added default name handling

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
+        String name;
+
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            name = args[0];
         } else {
-            System.out.println("Please provide a name");
+            name = "World";
         }
+
+        System.out.println("Hello " + name);
     }
 }
